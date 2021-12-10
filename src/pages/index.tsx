78e41bcd7724f8f16b2880265a6b53a396a4fc2c
bruno3du <bruno.3duardo@hotmail.com>
2021-12-09 23:58:02 -0300

    finalizando home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
 /** @format */
 
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
 	return (
-		<div>
+		<>
 			<Head>
-				<title>ig.news | Home</title>
+				<title>Home | ig.news</title>
 			</Head>
-			<h1>Hello World</h1>
-		</div>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>for $9.90 month</span>
+					</p>
+				</section>
+				<Image
+					src='/images/avatar.svg'
+					alt='Girl coding'
+					width='336px'
+					height='521px'
+				/>
+			</main>
+		</>
 	);
 }
